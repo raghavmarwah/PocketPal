87bea14a231434d067eb2954ee29084e3e047155
raghavmarwah <raghavmarwah98@hotmail.com>
2017-07-04 18:34:44 -0700

    Updated bottom navigation bar, changed app theme colors!

--- app/src/main/java/com/raghavmarwah/pocketpal/MonthViewActivity.java
+++ app/src/main/java/com/raghavmarwah/pocketpal/MonthViewActivity.java
@@ -5,11 +5,16 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MonthViewActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    RelativeLayout homeLayout;
+    RelativeLayout analystLayout;
+    RelativeLayout calendarLayout;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,9 +22,20 @@ public class MonthViewActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigationHome:
+                    homeLayout.setVisibility(View.VISIBLE);
+                    analystLayout.setVisibility(View.INVISIBLE);
+                    calendarLayout.setVisibility(View.INVISIBLE);
                     return true;
-                case R.id.navigation_analyst:
+                case R.id.navigationAnalyst:
+                    homeLayout.setVisibility(View.INVISIBLE);
+                    analystLayout.setVisibility(View.VISIBLE);
+                    calendarLayout.setVisibility(View.INVISIBLE);
+                    return true;
+                case R.id.navigationCalendar:
+                    homeLayout.setVisibility(View.INVISIBLE);
+                    analystLayout.setVisibility(View.INVISIBLE);
+                    calendarLayout.setVisibility(View.VISIBLE);
                     return true;
             }
             return false;
@@ -32,8 +48,14 @@ public class MonthViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_month_view);
 
+        //Bottom navigation bar
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //Object Variables
+        homeLayout = (RelativeLayout) findViewById(R.id.homeLayout);
+        analystLayout = (RelativeLayout) findViewById(R.id.analystLayout);
+        calendarLayout = (RelativeLayout) findViewById(R.id.calendarLayout);
     }
 
 }

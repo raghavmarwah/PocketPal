a85f4a9b7fea6ebb547f1e5f0d8224b9a1ad1b22
raghavmarwah <raghavmarwah98@hotmail.com>
2017-07-04 17:16:59 -0700

    Bottom navigation options updated

--- app/src/main/java/com/raghavmarwah/pocketpal/MonthViewActivity.java
+++ app/src/main/java/com/raghavmarwah/pocketpal/MonthViewActivity.java
@@ -18,13 +18,8 @@ public class MonthViewActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_analyst:
                     return true;
             }
             return false;
@@ -37,7 +32,6 @@ public class MonthViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_month_view);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }

4c2e2b50d6e4e67d849fd0a9074926b4b66ebf67
raghavmarwah <raghavmarwah98@hotmail.com>
2017-07-24 17:24:55 -0700

    HomeLayout completed with expenditure views and progress bars!

--- app/src/main/java/com/raghavmarwah/pocketpal/MonthViewActivity.java
+++ app/src/main/java/com/raghavmarwah/pocketpal/MonthViewActivity.java
@@ -8,15 +8,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MonthViewActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-    RelativeLayout homeLayout;
+    LinearLayout homeLayout;
     RelativeLayout analystLayout;
     RelativeLayout calendarLayout;
+    RelativeLayout profileLayout;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -28,16 +30,25 @@ public class MonthViewActivity extends AppCompatActivity {
                     homeLayout.setVisibility(View.VISIBLE);
                     analystLayout.setVisibility(View.INVISIBLE);
                     calendarLayout.setVisibility(View.INVISIBLE);
+                    profileLayout.setVisibility(View.INVISIBLE);
                     return true;
                 case R.id.navigationAnalyst:
                     homeLayout.setVisibility(View.INVISIBLE);
                     analystLayout.setVisibility(View.VISIBLE);
                     calendarLayout.setVisibility(View.INVISIBLE);
+                    profileLayout.setVisibility(View.INVISIBLE);
                     return true;
                 case R.id.navigationCalendar:
                     homeLayout.setVisibility(View.INVISIBLE);
                     analystLayout.setVisibility(View.INVISIBLE);
                     calendarLayout.setVisibility(View.VISIBLE);
+                    profileLayout.setVisibility(View.INVISIBLE);
+                    return true;
+                case R.id.navigationProfile:
+                    homeLayout.setVisibility(View.INVISIBLE);
+                    analystLayout.setVisibility(View.INVISIBLE);
+                    calendarLayout.setVisibility(View.INVISIBLE);
+                    profileLayout.setVisibility(View.VISIBLE);
                     return true;
             }
             return false;
@@ -53,11 +64,13 @@ public class MonthViewActivity extends AppCompatActivity {
         //Bottom navigation bar
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationViewHelper.disableShiftMode(navigation);
 
         //Layout Variables
-        homeLayout = (RelativeLayout) findViewById(R.id.homeLayout);
+        homeLayout = (LinearLayout) findViewById(R.id.homeLayout);
         analystLayout = (RelativeLayout) findViewById(R.id.analystLayout);
         calendarLayout = (RelativeLayout) findViewById(R.id.calendarLayout);
+        profileLayout = (RelativeLayout) findViewById(R.id.profileLayout);
 
         //Object Variables
         Button addExpenditure = (Button) findViewById(R.id.addExpButton);

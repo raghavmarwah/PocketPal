72b360f603e1f9b0a8b4387341a42203eb3420c5
raghavmarwah <raghavmarwah98@hotmail.com>
2017-07-31 18:25:57 -0700

    AutoTransition for GetStartedActivity enabled..

--- app/src/main/java/com/csis3175/pocketpal/GetStartedActivity.java
+++ app/src/main/java/com/csis3175/pocketpal/GetStartedActivity.java
@@ -3,10 +3,14 @@ package com.csis3175.pocketpal;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+
+import java.util.concurrent.TimeUnit;
 
 public class GetStartedActivity extends AppCompatActivity {
 
@@ -22,34 +26,52 @@ public class GetStartedActivity extends AppCompatActivity {
         MyDB db = new MyDB(this);
         final SQLiteDatabase rdb = db.getReadableDatabase();
 
-        Button next = (Button) findViewById(R.id.get_started_button);
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        String selectQuery = "SELECT COUNT(*) FROM " + UserEntry.TABLE_NAME;
+        int check = 0;
+        try {
+            Cursor cursor = rdb.rawQuery(selectQuery, null);
+            if (cursor != null) {
+                cursor.moveToFirst();
+                check = Integer.parseInt(cursor.getString(0));
+            }
 
-                String selectQuery = "SELECT COUNT(*) FROM " + UserEntry.TABLE_NAME;
-                int check = 0;
-                try {
-                    Cursor cursor = rdb.rawQuery(selectQuery, null);
-                    if (cursor != null) {
-                        cursor.moveToFirst();
-                        check = Integer.parseInt(cursor.getString(0));
-                    }
+        } catch (Exception ex) {}
 
-                } catch (Exception ex) {}
+        if(check==0) {
 
-                if(check==0) {
-                    Intent myIntent = new Intent(GetStartedActivity.this, AddUserInfoActivity.class);
-                    startActivity(myIntent);
-                    finish();
+            Button next = (Button) findViewById(R.id.get_started_button);
+            next.setVisibility(View.VISIBLE);
+            next.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                Intent myIntent = new Intent(GetStartedActivity.this, AddUserInfoActivity.class);
+                startActivity(myIntent);
+                finish();
+
                 }
-                else{
+            });
+        }
+        else{
+
+            ProgressBar progressIndeterminate = (ProgressBar) findViewById(R.id.progressIndeterminate);
+            progressIndeterminate.setVisibility(View.VISIBLE);
+
+            new CountDownTimer(1000,1000){
+                @Override
+                public void onTick(long millisUntilFinished){}
+
+                @Override
+                public void onFinish(){
+
                     Intent myIntent = new Intent(GetStartedActivity.this,MonthViewActivity.class);
                     startActivity(myIntent);
                     finish();
+
                 }
-            }
-        });
+            }.start();
+        }
+
     }
 
 }
